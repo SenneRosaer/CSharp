f54395b4eddc0e395748b47bead3ec73c26b735e
SenneRosaer <sennerosaer@SenneRosaer-Vast.localdomain>
2020-06-30 12:41:01 +0200

    First Two projects

---
+++ E/E/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace E
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            string input = Console.ReadLine();
+            int input_int;
+            Int32.TryParse(input, out input_int);
+            Console.WriteLine(Math.Round(Math.E, input_int));
+        }
+    }
+}
---
+++ Pi2/Pi2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Pi2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            string input = Console.ReadLine();
+            int input_int;
+            Int32.TryParse(input, out input_int);
+            double result = Math.Round(Math.PI, input_int);
+            Console.WriteLine(result);
+        }
+    }
+}
